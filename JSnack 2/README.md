2c34b4fef7251318041d1630f9de662138cd2070
JacopoLombardi <lombajacopo@gmail.com>
2024-03-07 16:21:47 +0100

    [add] Snack 2 | task 1 completed

--- JSnack 2/README.md
+++ JSnack 2/README.md
@@ -1,2 +1,8 @@
 Js Snack 2
 ===
+#### Es. Crea 10 oggetti che rappresentano una zucchina. Dividi in due array separati le zucchine che misurano meno o più di 15cm. Infine stampa separatamente quanto pesano i due gruppi di zucchine.
+
+1. Creare 10 oggettti, il quale ciascuno avrà la proprietà (varietà, peso, lunghezza).
+2. 
+3. 
+4. 
--- JSnack 2/js/script.js
+++ JSnack 2/js/script.js
@@ -1,2 +1,66 @@
 
+// 10 oggetti 'zucchina'
+const zucchina1 = {
+   varietà: 'rossa',
+   peso: '2Kg',
+   lunghezza: '10cm'
+   };
 
+const zucchina2 = {
+   varietà: 'viola',
+   peso: '1Kg',
+   lunghezza: '12cm'
+   };
+
+const zucchina3 = {
+   varietà: 'marrone',
+   peso: '2.5Kg',
+   lunghezza: '15cm'
+   };
+
+const zucchina4 = {
+   varietà: 'bianca',
+   peso: '1.5Kg',
+   lunghezza: '20cm'
+   };
+
+const zucchina5 = {
+   varietà: 'verde',
+   peso: '0.5Kg',
+   lunghezza: '16cm'
+   };
+
+const zucchina6 = {
+   varietà: 'gialla',
+   peso: '1.8Kg',
+   lunghezza: '8cm'
+   };
+
+const zucchina7 = {
+   varietà: 'blu',
+   peso: '1.2Kg',
+   lunghezza: '15cm'
+   };
+
+const zucchina8 = {
+   varietà: 'nera',
+   peso: '1.4Kg',
+   lunghezza: '17cm'
+   };
+
+const zucchina9 = {
+   varietà: 'rosa',
+   peso: '3.3Kg',
+   lunghezza: '12cm'
+   };
+
+const zucchina10 = {
+   varietà: 'azzurra',
+   peso: '5.4Kg',
+   lunghezza: '10cm'
+   };
+
+
+
+
+   
