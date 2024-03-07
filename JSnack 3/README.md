a9f48271910c08eeacce0137bc5bf570640a542e
JacopoLombardi <lombajacopo@gmail.com>
2024-03-07 17:19:55 +0100

    [add] Snack 3 | Finished

--- JSnack 3/README.md
+++ JSnack 3/README.md
@@ -1,2 +1,7 @@
 Js Snack 3
 ===
+#### Es. Scrivi una funzione che accetti una stringa come argomento e la ritorni girata (es. Ciao -> oaiC).
+
+1. Ho una parola in ingresso.
+2. tramite una funzione rigiro la parola.
+3. la parola torna fuori dalla funzione al contrario.
--- JSnack 3/js/script.js
+++ JSnack 3/js/script.js
@@ -1,2 +1,20 @@
 
+// variabili
+const output  = document.querySelector('._output');
+const parola = 'ciao';
 
+
+
+// output
+output.innerHTML += reverseWord(parola);
+
+
+
+
+// funzione che rigira la parola
+function reverseWord(word){
+
+   // divido la parola in lettere e le metto in un array, rigiro le lettere, ricompongo la parola in una stringa
+   const reversedWord = word.split('').reverse().join('');
+   return reversedWord;
+};
