adda868a2e9eec6d87e73ef6b08808a8942913f2
JacopoLombardi <lombajacopo@gmail.com>
2024-03-07 17:48:18 +0100

    [add] Snack 4 | Finished

--- JSnack 4/README.md
+++ JSnack 4/README.md
@@ -1,2 +1,7 @@
 Js Snack 4
 ===
+#### Es. Scrivi una funzione che fonda due array (aventi lo stesso numero di elementi) prendendo alternativamente gli elementi da uno e dall’altro esempio [a,b,c], [1,2,3] → [a,1,b,2,c,3].
+
+1. Creare 2 tipi di array uno composto da lettere e uno da numeri.
+2. Dentro una funzione creo un ciclo 'for' dove con l'indice, ad ogni ciclo leggo un elemento di 'lettereArray' e poi un elemento di 'numeriArray', e così via.
+3. Mostro in console l'array mischiato.
--- JSnack 4/js/script.js
+++ JSnack 4/js/script.js
@@ -1,2 +1,25 @@
 
+// array 
+const lettereArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'l'];
 
+const numeriArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const letterePlusNumeriArray = [];
+
+
+// invoco la funzione
+shakeArray(lettereArray, numeriArray);
+
+
+
+// funzione per mischiare i 2 array
+function shakeArray(lettere, numeri){
+   
+   for(let i = 0; i < 10; i++){
+      letterePlusNumeriArray.push(lettere[i]);
+      letterePlusNumeriArray.push(numeri[i]);
+   }
+}
+   
+
+console.log(letterePlusNumeriArray)
