40dac1d3b3c443b31d3712d271e546b13cdf3fa8
JacopoLombardi <lombajacopo@gmail.com>
2024-03-07 15:29:48 +0100

    [add] Snack 1 | task 1 completed

--- JSnack 1/README.md
+++ JSnack 1/README.md
@@ -1,2 +1,10 @@
 Js Snack 1
 ===
+#### Es. Crea un array di 10 oggetti che rappresentano una zucchina, indicando per ognuna varietà, peso e lunghezza. Calcola quanto pesano tutte le zucchine.
+
+1. Creare un Array di 10 oggetti, i quali hanno ciascuno 3 proprietà (varietà, peso, lunghezza).
+2. 
+3. 
+4. 
+5. 
+
--- JSnack 1/js/script.js
+++ JSnack 1/js/script.js
@@ -1,2 +1,60 @@
 
+// array di oggetti
+const elencoZucchine = [
+   {
+      varietà: 'rossa',
+      peso: '2Kg',
+      lunghezza: '10cm'
+   },
+   {
+      varietà: 'viola',
+      peso: '1Kg',
+      lunghezza: '12cm'
+   },
+   {
+      varietà: 'marrone',
+      peso: '2.5Kg',
+      lunghezza: '15cm'
+   },
+   {
+      varietà: 'bianca',
+      peso: '1.5Kg',
+      lunghezza: '20cm'
+   },
+   {
+      varietà: 'verde',
+      peso: '0.5Kg',
+      lunghezza: '16cm'
+   },
+   {
+      varietà: 'gialla',
+      peso: '1.8Kg',
+      lunghezza: '8cm'
+   },
+   {
+      varietà: 'blu',
+      peso: '1.2Kg',
+      lunghezza: '15cm'
+   },
+   {
+      varietà: 'nera',
+      peso: '1.9Kg',
+      lunghezza: '17cm'
+   },
+   {
+      varietà: 'rosa',
+      peso: '3.3Kg',
+      lunghezza: '12cm'
+   },
+   {
+      varietà: 'azzurra',
+      peso: '5.4Kg',
+      lunghezza: '10cm'
+   },
+];
+
+
+
+
+
 
